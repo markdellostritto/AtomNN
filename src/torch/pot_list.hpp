8f7b0522d82a3e73d23cdf431bb3779dba21bcba
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2023-06-02 16:25:03 -0400

    Add files via upload

---
+++ src/torch/pot_list.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef POT_LIST_HPP
+#define POT_LIST_HPP
+
+#include "src/torch/pot_coul_cut.hpp"
+#include "src/torch/pot_coul_long.hpp"
+#include "src/torch/pot_coul_wolf.hpp"
+#include "src/torch/pot_coul_dsf.hpp"
+#include "src/torch/pot_gauss_cut.hpp"
+#include "src/torch/pot_gauss_dsf.hpp"
+#include "src/torch/pot_gauss_long.hpp"
+#include "src/torch/pot_lj_cut.hpp"
+#include "src/torch/pot_lj_long.hpp"
+#include "src/torch/pot_ldamp_cut.hpp"
+#include "src/torch/pot_ldamp_long.hpp"
+#include "src/torch/pot_qeq_gl.hpp"
+#include "src/torch/pot_nnpe.hpp"
+#include "src/torch/pot_nnpeq.hpp"
+
+#endif
