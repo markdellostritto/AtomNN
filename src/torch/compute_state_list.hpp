d1ae536363ec2bd8536fb9b80b577915722a9a24
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2023-06-02 16:24:03 -0400

    Add files via upload

---
+++ src/torch/compute_state_list.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef COMPUTE_STATE_LIST_HPP
+#define COMPUTE_STATE_LIST_HPP
+
+#include "src/torch/compute_state_ke.hpp"
+#include "src/torch/compute_state_pe.hpp"
+#include "src/torch/compute_state_te.hpp"
+#include "src/torch/compute_state_temp.hpp"
+
+#endif
---
+++ src/torch/compute_state_pe.cpp
@@ -0,0 +1,21 @@
+#include "src/torch/compute_state_pe.hpp"
+
+namespace compute{
+
+namespace state{
+
+//==== operators ====
+
+std::ostream& operator<<(std::ostream& out, const PE& pe){
+	return static_cast<const Base&>(pe);
+}
+
+//==== member functions ====
+
+double PE::compute(const Struc& struc){
+	return struc.pe();
+}
+
+}
+
+}
